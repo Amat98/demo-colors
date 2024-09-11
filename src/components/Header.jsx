63feeda4f8946a98cf84a9f798d644d8ac8b5d21
Amat98 <milton.yv98@gmail.com>
2024-09-11 09:38:53 -0500

    holaaaaaaa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
         <h1 className="color--titles max-w-2xl mx-auto text-5xl rounded-md  p-2 font-semibold text-center">
           Bienvenid@ a <span className="font-bold bg-transparent">Resuelve</span><span className="font-bold">Ya!!</span>
         </h1>
-        <h2 className="color--titles rounded-md p-2 max-w-xl mx-auto text-4xl mt-1  text-center">Nosotros si, <span className="font-bold">resolvemos!</span></h2>
-        <h3 className="color--titles rounded-md p-2 max-w-3xl text-3xl mt-2  text-center">
+        <h2 className="color--titles rounded-md p-2 max-w-xl mx-auto text-4xl mt-1 font-semibold  text-center">Nosotros si, <span className="font-bold">resolvemos!</span></h2>
+        <h3 className="color--titles rounded-md font-light p-2 max-w-3xl text-3xl mt-2 text-center">
           En <span className="text-white bg-transparent font-bold">Resuelve</span><span className="font-bold">Ya!</span>, te conectamos con profesionales calificados para los
           servicios que necesitas, cuando los necesitas. Nuestra plataforma te
           permite encontrar <span>e</span>lectricistas, <span>p</span>intores, <span>f</span>ontaneros y muchos más, de
           manera rápida y sencilla.
         </h3>
       </div>
-      <div className="bg-black/70 h-[400px] w-[90%] max-w-[1100px] absolute rounded-full shadow"></div>
+      <div className="bg-black/50 h-[400px] w-[90%] max-w-[1100px] absolute rounded-full shadow"></div>
     </header>
   );
 };
