f45af7389ef8ffe57e4a749ce56f27e51faa9bbc
Omar <milton.yv98@gmail.com>
2024-09-11 08:21:10 -0500

    demo de colores home

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main >
+      <h1 className="text-3xl text-center p-4">Aqui ira el contenido principal</h1>
+      <section>
+        
+      </section>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav className="flex z-10 justify-between items-center p-3 fixed min-w-full backdrop-blur-sm">
+      <div className="bg-transparent"><h1 className="text-3xl font-bold"><span className="bg-white logo">Resuelve</span><span>Ya!</span></h1></div>
+
+      <form className="w-[50%] max-w-xl">
+        <input type="text" className="outline-none rounded-full w-full max-w-xl: text-black bg-white/95 p-1 px-5" placeholder="Que estas buscando...?"/>
+      </form>
+
+      <ul className="flex gap-3 list-none text-xl font-bold ">
+        <li className="">INICAR SESION</li>
+        <li>REGISTRARSE</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
